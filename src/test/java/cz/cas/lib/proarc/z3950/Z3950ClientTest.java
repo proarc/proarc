cf923d49c3315845da54f4a8bb586cd1478c6854
Jan Pokorsky <pokorsky@gmail.com>
2012-10-01 17:18:21 +0200

    initial version of Z39.50 client

---
+++ src/test/java/cz/cas/lib/proarc/z3950/Z3950ClientTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2012 Jan Pokorsky
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cas.lib.proarc.z3950;
+
+import java.io.StringWriter;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+/**
+ * See src/test/resources/log4j.properties to configure jzkit logging
+ *
+ * @author Jan Pokorsky
+ */
+public class Z3950ClientTest {
+    private String host;
+    private String port;
+    private String base;
+
+    public Z3950ClientTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        host = System.getProperty("cz.cas.lib.proarc.z3950.Z3950ClientTest.host");
+        port = System.getProperty("cz.cas.lib.proarc.z3950.Z3950ClientTest.port");
+        base = System.getProperty("cz.cas.lib.proarc.z3950.Z3950ClientTest.base");
+        Assume.assumeNotNull(host, port, base);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSearch() throws Exception {
+        String query = System.getProperty("cz.cas.lib.proarc.z3950.Z3950ClientTest.query");
+        Assume.assumeNotNull(query);
+        Z3950Client client = new Z3950Client(host, Integer.parseInt(port), base);
+        try {
+            Iterable<byte[]> search = client.search(query);
+            assertNotNull(search);
+            assertTrue(search.iterator().hasNext());
+            Transformer t = TransformerFactory.newInstance().newTransformer();
+            for (byte[] content : search) {
+                Document marcxml = Z3950Client.toMarcXml(content, "cp1250");
+                StringWriter dump = new StringWriter();
+                t.transform(new DOMSource(marcxml), new StreamResult(dump));
+                System.out.println("MarcXML:\n" + dump);
+//                LOG.info("MarcXML:\n" + dump);
+            }
+        } finally {
+            client.close();
+        }
+
+    }
+
+}
