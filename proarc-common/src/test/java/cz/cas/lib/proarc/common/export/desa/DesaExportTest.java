3d48036da625d3322c2f1e1b58b7b1156ab68dea
Robert Simonovsky <rsimonovsky@gmail.com>
2014-01-24 11:48:52 +0100

    additional test for derFile export added

--- proarc-common/src/test/java/cz/cas/lib/proarc/common/export/desa/DesaExportTest.java
+++ proarc-common/src/test/java/cz/cas/lib/proarc/common/export/desa/DesaExportTest.java
@@ -50,7 +50,7 @@ public class DesaExportTest {
 
     static {
         testParentMap.put("uuid:619dbb60-fada-4abd-b2ce-3144c086327d", "uuid:d18329c6-2f2a-48b3-b315-0da2a896ba3e");
-        testParentMap.put("uuid:00134047-30f3-4967-9966-2cdb606c8ac9", "uuid:00134047-30f3-4967-9966-2cdb606c8ac9");
+        testParentMap.put("uuid:00134047-30f3-4967-9966-2cdb606c8ac9", "uuid:619dbb60-fada-4abd-b2ce-3144c086327d");
     }
 
     @Rule
@@ -117,6 +117,45 @@ public class DesaExportTest {
             DesaElement.getElement(object, null, desaContext, true);
             DesaElementVisitor desaVisitor = new DesaElementVisitor();
             desaContext.getRootElement().accept(desaVisitor);
+        } catch (MetsExportException ex) {
+            assertEquals(0, ex.getExceptions().size());
+        }
+        String outputDir = extractFile(resultDir + File.separator + "A-37_FILE.zip", "correctDesaTest");
+        Mets mets = readMets(outputDir + File.separator + "mets.xml");
+        String DMDID = mets.getDmdSec().get(0).getID();
+        assertEquals("DM_0001", DMDID);
+        String structType = mets.getStructMap().get(0).getDiv().getTYPE();
+        assertEquals("file", structType);
+        String innerStructType = mets.getStructMap().get(0).getDiv().getDiv().get(0).getTYPE();
+        assertEquals("record", innerStructType);
+        String href = mets.getStructMap().get(0).getDiv().getDiv().get(0).getMptr().get(0).getHref();
+        assertEquals("A-37_01", href);
+        outputDir = extractFile(resultDir + File.separator + "A-37_01.zip", "noFileNameDesaTest");
+        mets = readMets(outputDir + File.separator + "mets.xml");
+        FileType fileType = mets.getFileSec().getFileGrp().get(0).getFile().get(0);
+        assertEquals("b5100365bd0c93d1596e29fb8e8969a0", fileType.getCHECKSUM());
+        assertEquals("kniha_text.tif", fileType.getFLocat().get(0).getHref());
+        File file = new File(outputDir + File.separator + fileType.getFLocat().get(0).getHref());
+        assertEquals(true, file.exists());
+    }
+
+    @Test
+    public void testSaveCorrectDesafromFile() throws Exception {
+        String directory = "correctDesa";
+        copyFiles("correctDesa", "correctDesa.zip");
+        String sourceDirPath = tmp.getRoot().getAbsolutePath() + File.separator + directory + File.separator;
+        File resultDir = tmp.newFolder("result" + directory);
+        String path = sourceDirPath + "00134047-30f3-4967-9966-2cdb606c8ac9.xml";
+        DesaContext desaContext = new DesaContext();
+        desaContext.setFsParentMap(testParentMap);
+        desaContext.setOutputPath(resultDir.getAbsolutePath());
+        desaContext.setPackageID("SAMPLEDESA");
+        desaContext.setPath(sourceDirPath);
+        try {
+            DigitalObject object = MetsUtils.readFoXML(path);
+            DesaElement desaElement = DesaElement.getElement(object, null, desaContext, true);
+            DesaElementVisitor desaVisitor = new DesaElementVisitor();
+            desaElement.accept(desaVisitor);
         } catch (MetsExportException ex) {
             assertEquals(0, ex.getExceptions().size());
         }
