6f88a6f4040916e9ecd0a70393d3633940e66e58
Jan Pokorsky <pokorsky@gmail.com>
2014-02-17 11:59:23 +0100

    Remove unused and invalid parseMetsForSipId from SIP2DESATransporter.

--- proarc-desa/src/main/java/cz/cas/lib/proarc/desa/SIP2DESATransporter.java
+++ proarc-desa/src/main/java/cz/cas/lib/proarc/desa/SIP2DESATransporter.java
@@ -24,7 +24,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
-import java.util.Enumeration;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.logging.FileHandler;
@@ -32,16 +31,12 @@ import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamReader;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.ws.Holder;
 
@@ -359,8 +354,7 @@ public final class SIP2DESATransporter {
      * @param writeResults
      */
     private void uploadFile(File file, SipType sipType, boolean writeResults) {
-        Holder<String> sipId = new Holder<String>(file.getName().replace(".zip", ""));
-        // Holder<String> sipId = new Holder<String>(getSipId(file));
+         Holder<String> sipId = new Holder<String>(getSipId(file));
         Holder<String> idSipVersion = new Holder<String>();
         String checksum = getMD5Checksum(file);
         log.info("Transporting file: " + file.getName());
@@ -408,43 +402,7 @@ public final class SIP2DESATransporter {
     }
 
     private String getSipId(File sipFile) {
-        String retval = null;
-        try {
-            ZipFile file = new ZipFile(sipFile);
-            try {
-                final Enumeration<? extends ZipEntry> entries = file.entries();
-                while (entries.hasMoreElements()) {
-                    final ZipEntry entry = entries.nextElement();
-                    if ("mets.xml".equalsIgnoreCase(entry.getName())) {
-                        retval = parseMetsForSipId(file.getInputStream(entry));
-                    }
-                }
-            } finally {
-                file.close();
-            }
-        } catch (Exception ex) {
-            throw new RuntimeException("Cannot read sipId:", ex);
-        }
-        if (retval == null || "".equals(retval)) {
-            throw new RuntimeException("No dc:identifier found in file: " + sipFile.getName());
-        }
-        return retval;
-    }
-
-    private String parseMetsForSipId(InputStream input) throws Exception {
-        String retval = null;
-        XMLInputFactory f = XMLInputFactory.newInstance();
-        XMLStreamReader r = f.createXMLStreamReader(input);
-        while (r.hasNext()) {
-            r.next();
-            if (r.isStartElement()) {
-                if ("identifier".equalsIgnoreCase(r.getName().getLocalPart()) && "dc".equalsIgnoreCase(r.getName().getPrefix())) {
-                    retval = r.getElementText();
-                }
-
-            }
-        }
-        return retval;
+        return sipFile.getName().replace(".zip", "");
     }
 
     /**
