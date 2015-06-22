230dc06c233878fc25646cf0816963197ca01b63
Jan Pokorsky <pokorsky@gmail.com>
2015-06-22 13:33:36 +0200

    Add GenericExternalProcess.

---
+++ proarc-common/src/main/java/cz/cas/lib/proarc/common/process/GenericExternalProcess.java
@@ -0,0 +1,387 @@
+/*
+ * Copyright (C) 2015 Jan Pokorsky
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cas.lib.proarc.common.process;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ * The experimental implementation of an external process that can be customized
+ * in more details including input and output parameters.
+ *
+ * @author Jan Pokorsky
+ */
+public class GenericExternalProcess extends ExternalProcess {
+
+    public static final String DST_PARENT = "output.folder";
+    public static final String DST_PATH = "output.file";
+    public static final String SRC_EXT = "input.file.ext";
+    public static final String SRC_NAME = "input.file.name";
+    public static final String SRC_NAME_EXT = "input.file.nameExt";
+    public static final String SRC_PATH = "input.file";
+    public static final String SRC_PARENT = "input.folder";
+
+    private File inputFile;
+    private File outputFile;
+    private final ParamHandler parameters;
+    private final Configuration conf;
+    private static final Pattern REPLACE_PARAM_PATTERN = Pattern.compile("\\$\\{([^}]+)\\}");
+    private boolean skippedProcess;
+    private ProcessResult processResult;
+
+    public GenericExternalProcess(Configuration conf) {
+        super(conf);
+        this.conf = conf;
+        this.parameters = new ParamHandler();
+    }
+
+    public GenericExternalProcess addInputFile(File file) {
+        this.inputFile = file;
+        parameters.addInputFile(file);
+        return this;
+    }
+
+    public File getInputFile() {
+        return inputFile;
+    }
+
+    public GenericExternalProcess addOutputFile(File file) {
+        this.outputFile = file;
+        parameters.addOutput(file);
+        return this;
+    }
+
+    public File getOutputFile() {
+        return processResult == null ? outputFile : getResult().getOutputFile();
+    }
+
+    public ParamHandler getParameters() {
+        return parameters;
+    }
+
+    public Map<String, String> getResultParameters() {
+        return getResult().getParameters();
+    }
+
+    public ProcessResult getResult() {
+        if (processResult == null) {
+            throw new IllegalStateException("The process has not run yet!");
+        }
+        return processResult;
+    }
+
+    @Override
+    public void run() {
+        Configuration run = conf.subset("run");
+        String[] ifNotExists = run.getStringArray("if.notExists");
+        for (String ifNotExist : ifNotExists) {
+            String path = interpolateParameters(ifNotExist, parameters.getMap());
+            File file = new File(path);
+            if (file.exists()) {
+                skippedProcess = true;
+                break;
+            }
+        }
+        if (!skippedProcess) {
+            super.run();
+        }
+        processResult = ProcessResult.getResultParameters(conf,
+                parameters.getMap(), isSkippedProcess(), getExitCode(), getFullOutput());
+    }
+
+    public boolean isSkippedProcess() {
+        return skippedProcess;
+    }
+
+    @Override
+    protected List<String> buildCmdLine(Configuration conf) {
+        List<String> cmdLine = super.buildCmdLine(conf);
+        interpolateParameters(cmdLine);
+        return cmdLine;
+    }
+
+    /**
+     * Interpolate parameter values. The replace pattern is {@code ${name}}.
+     * @param s a string to search for placeholders
+     * @return the resolved string
+     * @see #addParameter(java.lang.String, java.lang.String)
+     */
+    static String interpolateParameters(String s, Map<String, String> parameters) {
+        if (s == null || s.length() < 4 || parameters.isEmpty()) { // minimal replaceable value ${x}
+            return s;
+        }
+        // finds ${name} patterns
+        Matcher m = REPLACE_PARAM_PATTERN.matcher(s);
+        StringBuffer sb = null;
+        while(m.find()) {
+            if (m.groupCount() == 1) {
+                String param = m.group(1);
+                String replacement = parameters.get(param);
+                if (replacement != null) {
+                    sb = sb != null ? sb : new StringBuffer();
+                    m.appendReplacement(sb, Matcher.quoteReplacement(replacement));
+                }
+            }
+        }
+        if (sb == null) {
+            return s;
+        }
+        m.appendTail(sb);
+        return sb.toString();
+    }
+
+    void interpolateParameters(List<String> cmdLine) {
+        if (parameters.isEmpty()) {
+            return ;
+        }
+        for (ListIterator<String> it = cmdLine.listIterator(); it.hasNext();) {
+            it.set(interpolateParameters(it.next(), parameters.getMap()));
+        }
+    }
+
+    public static class ParamHandler {
+
+        private final Map<String, String> parameters = new HashMap<String, String>();
+
+        public ParamHandler add(Map<String, String> parameters) {
+            this.parameters.putAll(parameters);
+            return this;
+        }
+
+        public ParamHandler add(String name, String value) {
+            parameters.put(name, value);
+            return this;
+        }
+
+        public ParamHandler addInputFile(File file) {
+            String nameExt = file.getName();
+            return add(SRC_PATH, file.getAbsolutePath())
+                    .add(SRC_EXT, FilenameUtils.getExtension(nameExt))
+                    .add(SRC_NAME, FilenameUtils.getBaseName(nameExt))
+                    .add(SRC_NAME_EXT, nameExt)
+                    .add(SRC_PARENT, file.getParentFile().getPath());
+        }
+
+        public ParamHandler addOutput(File file) {
+            return add(DST_PATH, file.getAbsolutePath())
+                    .add(DST_PARENT, file.getParentFile().getPath());
+        }
+
+        public Map<String, String> getMap() {
+            return parameters;
+        }
+
+        public boolean isEmpty() {
+            return parameters.isEmpty();
+        }
+
+    }
+    
+    public static class ProcessResult {
+
+        private final Map<String, String> parameters;
+        private final ExitStrategy exit;
+        private final String processorId;
+
+        public ProcessResult(String processorId, Map<String, String> params, ExitStrategy exit) {
+            this.parameters = params;
+            this.exit = exit;
+            this.processorId = processorId;
+        }
+
+        public Map<String, String> getParameters() {
+            return parameters;
+        }
+
+        public File getOutputFile() {
+            String path = parameters.get(processorId + ".param." + DST_PATH);
+            if (path == null) {
+                path = parameters.get(DST_PATH);
+            }
+            return path == null || path.isEmpty() ? null : new File(path);
+        }
+
+        public ExitStrategy getExit() {
+            return exit;
+        }
+
+        /**
+         * Merges result parameters. {@code onExit} and {@code onSkip} are experimental features.
+         *
+         * @param conf
+         * @param inputParameters
+         * @param skippedProcess
+         * @param exitCode
+         * @param log
+         * @return
+         */
+        public static ProcessResult getResultParameters(Configuration conf,
+                Map<String, String> inputParameters, boolean skippedProcess,
+                int exitCode, String log) {
+
+            // gets <processorId>.param.* parameters with interpolated values
+            // it allows to share properties among process and read helper values from process declaration (mime, output file, ...)
+            String processorId = conf.getString("id");
+            Map<String, String> hm = new HashMap<String, String>(inputParameters);
+            addResultParamaters(conf, processorId, hm);
+
+            ExitStrategy exit = new ExitStrategy();
+            if (skippedProcess) {
+                Configuration onSkip = conf.subset("onSkip");
+                addResultParamaters(onSkip, processorId, hm);
+                exit.setSkip(true);
+                exit.setContinueWithProcessIds(Arrays.asList(onSkip.getStringArray("next")));
+                return new ProcessResult(processorId, hm, exit);
+            }
+
+            exit.setExitCode(exitCode);
+            String[] onExitIds = conf.getStringArray("onExits");
+            Configuration onExitConf = conf.subset("onExit");
+            boolean defaultExit = true;
+            for (String onExitId : onExitIds) {
+                if (isExitId(onExitId, exitCode)) {
+                    Configuration onExitIdConf = onExitConf.subset(onExitId);
+                    addResultParamaters(onExitIdConf, processorId, hm);
+                    exit.setErrorMessage(onExitIdConf.getString("message"));
+                    exit.setStop(onExitIdConf.getBoolean("stop", exitCode != 0));
+                    exit.setContinueWithProcessIds(Arrays.asList(onExitIdConf.getStringArray("next")));
+                    defaultExit = false;
+                    break;
+                }
+            }
+            if (defaultExit) {
+                exit.setStop(exitCode != 0);
+                exit.setErrorMessage(log);
+            }
+            return new ProcessResult(processorId, hm, exit);
+        }
+
+        private static Map<String, String> addResultParamaters(Configuration conf,
+                String processorId, Map<String, String> result) {
+
+            for (Iterator<String> it = conf.getKeys("param"); it.hasNext();) {
+                String param = it.next();
+                String value = interpolateParameters(conf.getString(param), result);
+                String resultName = processorId == null ? param : processorId + '.' + param;
+                result.put(resultName, value);
+            }
+            return result;
+        }
+
+        /**
+         * Resolves whether the {@code exitExpression} matches to {@code exitCode}.
+         * @param exitExpression syntax:
+         *      {@code '*' | '>' exitCode | '<' exitCode | exitCode [',' exitCode]*}
+         * @param exitCode
+         * @return
+         */
+        private static boolean isExitId(String exitExpression, int exitCode) {
+            boolean match = false;
+            if ("*".equals(exitExpression)) {
+                match = true;
+            } else if (exitExpression.charAt(0) == '>') {
+                try {
+                    int code = Integer.parseInt(exitExpression.substring(1));
+                    if (exitCode > code) {
+                        match = true;
+                    }
+                } catch (NumberFormatException ex) {
+                }
+            } else if (exitExpression.charAt(0) == '<') {
+                try {
+                    int code = Integer.parseInt(exitExpression.substring(1));
+                    if (exitCode < code) {
+                        match = true;
+                    }
+                } catch (NumberFormatException ex) {
+                }
+            } else {
+                String[] codes = exitExpression.split(",");
+                for (String codeStr : codes) {
+                    try {
+                        int code = Integer.parseInt(codeStr.trim());
+                        if (exitCode == code) {
+                            match = true;
+                        }
+                    } catch (NumberFormatException ex) {
+                    }
+                }
+            }
+            return match;
+        }
+    }
+
+    public static class ExitStrategy {
+
+        private Integer exitCode;
+        private boolean stop;
+        private boolean skip;
+        private List<String> continueWithProcessIds;
+        private String errorMessage;
+
+        public Integer getExitCode() {
+            return exitCode;
+        }
+
+        public void setExitCode(Integer exitCode) {
+            this.exitCode = exitCode;
+        }
+
+        public boolean isStop() {
+            return stop;
+        }
+
+        public void setStop(boolean stop) {
+            this.stop = stop;
+        }
+
+        public boolean isSkip() {
+            return skip;
+        }
+
+        public void setSkip(boolean skip) {
+            this.skip = skip;
+        }
+
+        public List<String> getContinueWithProcessIds() {
+            return continueWithProcessIds;
+        }
+
+        public void setContinueWithProcessIds(List<String> continueWithProcessIds) {
+            this.continueWithProcessIds = continueWithProcessIds;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+
+        public void setErrorMessage(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+    }
+
+}
---
+++ proarc-common/src/test/java/cz/cas/lib/proarc/common/process/GenericExternalProcessTest.java
@@ -0,0 +1,273 @@
+/*
+ * Copyright (C) 2015 Jan Pokorsky
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.cas.lib.proarc.common.process;
+
+import cz.cas.lib.proarc.common.CustomTemporaryFolder;
+import cz.cas.lib.proarc.common.imports.InputUtils;
+import cz.cas.lib.proarc.common.imports.TiffImporterTest;
+import cz.cas.lib.proarc.common.process.GenericExternalProcess.ParamHandler;
+import cz.cas.lib.proarc.common.process.GenericExternalProcess.ProcessResult;
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.configuration.BaseConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Assume;
+import org.junit.Rule;
+
+/**
+ *
+ * @author Jan Pokorsky
+ */
+public class GenericExternalProcessTest {
+
+    @Rule
+    public CustomTemporaryFolder temp = new CustomTemporaryFolder(true);
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testIMConvert() throws Exception {
+//        temp.setDeleteOnExit(false);
+        String imageMagicExec = "/usr/bin/convert";
+        Assume.assumeTrue(new File(imageMagicExec).exists());
+
+        File confFile = temp.newFile("props.cfg");
+        File root = temp.getRoot();
+        URL pdfaResource = TiffImporterTest.class.getResource("pdfa_test.pdf");
+        File pdfa = new File(root, "pdfa_test.pdf");
+        FileUtils.copyURLToFile(pdfaResource, pdfa);
+        FileUtils.writeLines(confFile, Arrays.asList(
+                "input.file.name=RESOLVED",
+                "exec=" + imageMagicExec,
+//                "arg=-verbose",
+                "arg=-thumbnail",
+                "arg=120x128",
+                "arg=$${input.file}[0]",
+                "arg=-flatten",
+                "arg=$${output.file}",
+                "id=test"
+        ));
+        PropertiesConfiguration conf = new PropertiesConfiguration(confFile);
+        GenericExternalProcess gep = new GenericExternalProcess(conf);
+        gep.addInputFile(pdfa);
+        File output = new File(root, "pdfa.jpg");
+        gep.addOutputFile(output);
+        gep.run();
+//        System.out.printf("#exit: %s, out: %s\nresults: %s\n",
+//                gep.getExitCode(), gep.getFullOutput(), gep.getResultParameters());
+        assertEquals("exit code", 0, gep.getExitCode());
+        assertTrue(output.toString(), output.exists());
+        assertTrue("Not JPEG", InputUtils.isJpeg(output));
+    }
+
+    /** onExit is an experimental feature .*/
+    @Test
+    public void testIMConvertOnExit() throws Exception {
+        String imageMagicExec = "/usr/bin/convert";
+        Assume.assumeTrue(new File(imageMagicExec).exists());
+        File confFile = temp.newFile("props.cfg");
+        File root = temp.getRoot();
+        URL pdfaResource = TiffImporterTest.class.getResource("pdfa_test.pdf");
+        File pdfa = new File(root, "pdfa_test.pdf");
+        FileUtils.copyURLToFile(pdfaResource, pdfa);
+        FileUtils.writeLines(confFile, Arrays.asList(
+                "input.file.name=RESOLVED",
+                "exec=" + imageMagicExec,
+                "arg=-thumbnail",
+                "arg=120x128",
+                "arg=$${input.file}[0]",
+                "arg=-flatten",
+                "arg=$${input.folder}/$${input.file.name}.jpg",
+                "result.file=$${input.folder}/$${input.file.name}.jpg",
+                "onExits=0",
+                "onExit.0.param.output.file=$${input.folder}/$${input.file.name}.jpg",
+                "id=test"
+        ));
+        PropertiesConfiguration conf = new PropertiesConfiguration(confFile);
+        GenericExternalProcess gep = new GenericExternalProcess(conf);
+        gep.addInputFile(pdfa);
+        gep.run();
+//        System.out.printf("#exit: %s, out: %s\nresults: %s\n",
+//                gep.getExitCode(), gep.getFullOutput(), gep.getResultParameters());
+        assertEquals("exit code", 0, gep.getExitCode());
+        File output = gep.getOutputFile();
+        assertNotNull(output);
+        assertTrue(output.toString(), output.exists());
+        assertTrue("Not JPEG", InputUtils.isJpeg(output));
+    }
+
+    @Test
+    public void testIMConvertFailure() throws Exception {
+        String imageMagicExec = "/usr/bin/convert";
+        Assume.assumeTrue(new File(imageMagicExec).exists());
+        File confFile = temp.newFile("props.cfg");
+        File root = temp.getRoot();
+        File pdfa = new File(root, "pdfa_test.pdf");
+        FileUtils.writeLines(confFile, Arrays.asList(
+                "input.file.name=RESOLVED",
+                "exec=" + imageMagicExec,
+                "arg=-thumbnail",
+                "arg=120x128",
+                "arg=$${input.file}[0]",
+                "arg=-flatten",
+                "arg=$${input.folder}/$${input.file.name}.jpg",
+                "onExits=0",
+                "onExit.0.param.output.file=$${input.folder}/$${input.file.name}.jpg",
+                "id=test"
+        ));
+        PropertiesConfiguration conf = new PropertiesConfiguration(confFile);
+        GenericExternalProcess gep = new GenericExternalProcess(conf);
+        gep.addInputFile(pdfa);
+        gep.run();
+//        System.out.printf("#exit: %s, out: %s\nresults: %s\n",
+//                gep.getExitCode(), gep.getFullOutput(), gep.getResultParameters());
+        assertEquals("exit code", 1, gep.getExitCode());
+        String outputPath = gep.getResultParameters().get("test.param." + GenericExternalProcess.DST_PATH);
+        assertNull(outputPath);
+    }
+
+    @Test
+    public void testEscapePropertiesConfiguration() throws Exception {
+        File confFile = temp.newFile("props.cfg");
+        FileUtils.writeLines(confFile, Arrays.asList(
+                "input.file.name=RESOLVED",
+                "-1=ERR",
+                "*=ERR2",
+                "1-3=ERR3",
+                ">1=ERR4",
+                "1,2,3=1\\,2\\,3",
+                "escape=-escape $${input.file.name}",
+                "resolve=-resolve ${input.file.name}[0]"
+        ));
+        PropertiesConfiguration conf = new PropertiesConfiguration(confFile);
+        assertEquals("ERR", conf.getString("-1"));
+        assertEquals("ERR2", conf.getString("*"));
+        assertEquals("ERR3", conf.getString("1-3"));
+        assertEquals("ERR4", conf.getString(">1"));
+        assertEquals("1,2,3", conf.getString("1,2,3"));
+        assertEquals("-escape ${input.file.name}", conf.getString("escape"));
+        assertEquals("-resolve RESOLVED[0]", conf.getString("resolve"));
+    }
+
+    @Test
+    public void testGetResultParameters() throws Exception {
+        File confFile = temp.newFile("props.cfg");
+        FileUtils.writeLines(confFile, Arrays.asList(
+                "processor.test.param.mime=image/jpeg",
+                "processor.test.onExits=0, >10, 2\\,3, *",
+                "processor.test.onExit.0.param.file=ERR_0\\:$${input.file.name}",
+                "processor.test.onExit.>10.param.file=ERR_>10\\:$${input.file.name}",
+                "processor.test.onExit.>10.stop=false",
+                "processor.test.onExit.2,3.param.file=ERR_2\\,3\\:$${input.file.name}",
+                "processor.test.onExit.*.param.file=ERR_*\\:$${input.file.name}",
+                "processor.test.onSkip.param.file=SKIPPED\\:$${input.file.name}"
+        ));
+        PropertiesConfiguration pconf = new PropertiesConfiguration(confFile);
+        String processorId = "processor.test";
+        Configuration conf = pconf.subset(processorId);
+        conf.setProperty("id", processorId);
+        GenericExternalProcess gep = new GenericExternalProcess(conf);
+        gep.addInputFile(confFile);
+        ProcessResult result = ProcessResult.getResultParameters(conf, gep.getParameters().getMap(), false, 0, null);
+        assertEquals("image/jpeg", result.getParameters().get(processorId + ".param.mime"));
+        assertEquals("ERR_0:props", result.getParameters().get(processorId + ".param.file"));
+        assertEquals(Integer.valueOf(0), result.getExit().getExitCode());
+        assertFalse(result.getExit().isStop());
+        assertFalse(result.getExit().isSkip());
+
+        result = ProcessResult.getResultParameters(conf, gep.getParameters().getMap(), false, 3, null);
+        assertEquals("image/jpeg", result.getParameters().get(processorId + ".param.mime"));
+        assertEquals("ERR_2,3:props", result.getParameters().get(processorId + ".param.file"));
+        assertEquals(Integer.valueOf(3), result.getExit().getExitCode());
+        assertTrue(result.getExit().isStop());
+        assertFalse(result.getExit().isSkip());
+
+        result = ProcessResult.getResultParameters(conf, gep.getParameters().getMap(), false, 11, null);
+        assertEquals("image/jpeg", result.getParameters().get(processorId + ".param.mime"));
+        assertEquals("ERR_>10:props", result.getParameters().get(processorId + ".param.file"));
+        assertEquals(Integer.valueOf(11), result.getExit().getExitCode());
+        assertFalse(result.getExit().isStop());
+        assertFalse(result.getExit().isSkip());
+
+        result = ProcessResult.getResultParameters(conf, gep.getParameters().getMap(), false, -1, null);
+        assertEquals("image/jpeg", result.getParameters().get(processorId + ".param.mime"));
+        assertEquals("ERR_*:props", result.getParameters().get(processorId + ".param.file"));
+        assertEquals(Integer.valueOf(-1), result.getExit().getExitCode());
+        assertTrue(result.getExit().isStop());
+        assertFalse(result.getExit().isSkip());
+
+        result = ProcessResult.getResultParameters(conf, gep.getParameters().getMap(), true, 0, null);
+        assertEquals("image/jpeg", result.getParameters().get(processorId + ".param.mime"));
+        assertEquals("SKIPPED:props", result.getParameters().get(processorId + ".param.file"));
+        assertFalse(result.getExit().isStop());
+        assertTrue(result.getExit().isSkip());
+    }
+
+    @Test
+    public void testAddInputFile() throws Exception {
+        final String inputName = "input.txt";
+        final File input = temp.newFile(inputName);
+        final BaseConfiguration conf = new BaseConfiguration();
+        conf.addProperty(ExternalProcess.PROP_EXEC, "test.sh");
+        conf.addProperty(ExternalProcess.PROP_ARG, "-i ${input.file.name}");
+        conf.addProperty(ExternalProcess.PROP_ARG, "-path ${input.file}[0]");
+        conf.addProperty(ExternalProcess.PROP_ARG, "-o ${input.folder}");
+        GenericExternalProcess gp = new GenericExternalProcess(conf).addInputFile(input);
+        assertEquals(gp.getParameters().getMap().get(GenericExternalProcess.SRC_NAME_EXT), input.getName());
+        assertEquals(gp.getParameters().getMap().get(GenericExternalProcess.SRC_PATH), input.getAbsolutePath());
+        assertEquals(gp.getParameters().getMap().get(GenericExternalProcess.SRC_PARENT), input.getParentFile().getAbsolutePath());
+        List<String> cmdLines = gp.buildCmdLine(conf);
+//        System.out.println(cmdLines.toString());
+    }
+
+    @Test
+    public void testInterpolateParameters() {
+        String testValue = "path=/${PATH1}/${PATH2}/${}";
+        ParamHandler paramHandler = new ParamHandler();
+        // no params
+        assertEquals(testValue, GenericExternalProcess.interpolateParameters(testValue, paramHandler.getMap()));
+
+        paramHandler.add("PATH1", "path1");
+        paramHandler.add("PATH2", "$2");
+        assertEquals("path=/path1/$2/${}", GenericExternalProcess.interpolateParameters(testValue, paramHandler.getMap()));
+    }
+
+}
