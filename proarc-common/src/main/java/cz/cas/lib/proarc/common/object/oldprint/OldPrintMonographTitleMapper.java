c483ae4ff077def20cfe15026597f618a977994e
Jakub Kremláček <jkremlacek@email.cz>
2017-06-16 12:33:35 +0200

    OldPrintMonographTitle removed dead code

--- proarc-common/src/main/java/cz/cas/lib/proarc/common/object/oldprint/OldPrintMonographTitleMapper.java
+++ proarc-common/src/main/java/cz/cas/lib/proarc/common/object/oldprint/OldPrintMonographTitleMapper.java
@@ -27,8 +27,5 @@ public class OldPrintMonographTitleMapper extends NdkMonographTitleMapper {
     @Override
     public void createMods(ModsDefinition mods, Context ctx) {
         super.createMods(mods, ctx);
-        //  mods/typeOfResource="text"
-//        OldPrintMapperUtils.addTypeOfResource(mods);
-        //  mods/genre="title"
     }
 }
