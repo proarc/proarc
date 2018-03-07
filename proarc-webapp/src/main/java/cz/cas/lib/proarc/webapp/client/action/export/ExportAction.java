d1d58ad718d3c31e16553923d7cc9b1302802bcf
Jakub Kremláček <jkremlacek@email.cz>
2018-03-07 12:58:24 +0100

    issue #711: Added notification window after requesting export

--- proarc-webapp/src/main/java/cz/cas/lib/proarc/webapp/client/action/export/ExportAction.java
+++ proarc-webapp/src/main/java/cz/cas/lib/proarc/webapp/client/action/export/ExportAction.java
@@ -22,6 +22,7 @@ import com.smartgwt.client.data.DSRequest;
 import com.smartgwt.client.data.DataSource;
 import com.smartgwt.client.data.Record;
 import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.util.SC;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.form.DynamicForm;
@@ -77,6 +78,7 @@ public abstract class ExportAction extends AbstractAction {
             setRequestOptions(record);
             dataSource.addData(record, dsCallback, dsRequest);
             d.destroy();
+            SC.say(i18n.ExportAction_Sent_Msg());
         });
 
         d.addCancelButton(() -> d.destroy());
