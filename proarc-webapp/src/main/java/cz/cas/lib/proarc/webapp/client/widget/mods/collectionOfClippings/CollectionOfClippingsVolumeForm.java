b5d0bc63eef14bad12896bde05bcf766713af96a
Lukáš Sýkora <sykora.lukas@email.cz>
2020-01-13 14:58:10 +0100

    issue #1063: new Plugin Collection of Clippings

---
+++ proarc-webapp/src/main/java/cz/cas/lib/proarc/webapp/client/widget/mods/collectionOfClippings/CollectionOfClippingsVolumeForm.java
@@ -0,0 +1,11 @@
+package cz.cas.lib.proarc.webapp.client.widget.mods.collectionOfClippings;
+
+import cz.cas.lib.proarc.webapp.client.widget.mods.NdkMonographVolumeForm;
+import cz.cas.lib.proarc.webapp.shared.form.Form;
+
+public class CollectionOfClippingsVolumeForm {
+
+    public Form build() {
+        return new NdkMonographVolumeForm().build();
+    }
+}
