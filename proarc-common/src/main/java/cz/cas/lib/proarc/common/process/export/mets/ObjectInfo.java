574318e4efd66ca4cec0990ace2f6ecd0e4c243b
LukasSykora <lukas.sykora@inovatika.cz>
2024-01-16 18:04:46 +0100

    issue #1428: premis pro eDocs

--- proarc-common/src/main/java/cz/cas/lib/proarc/common/process/export/mets/ObjectInfo.java
+++ proarc-common/src/main/java/cz/cas/lib/proarc/common/process/export/mets/ObjectInfo.java
@@ -3,11 +3,22 @@ package cz.cas.lib.proarc.common.process.export.mets;
 import edu.harvard.hul.ois.xml.ns.jhove.PropertyType;
 import edu.harvard.hul.ois.xml.ns.jhove.ValuesType;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ObjectInfo {
 
     private String applicationName;
     private String applicationVersion;
     private String applicationCreationDate;
+    private String objectCount;
+    private String pageCount;
+//    private String tableCount;
+//    private String graphicCount;
+    private String imageCoint;
+    private String language;
+    private Set<String> fonts;
+    private Set<String> filters;
 
     public ObjectInfo() {
     }
@@ -20,19 +31,90 @@ public class ObjectInfo {
                         if (!rootProperty.getValues().isEmpty()) {
                             for (PropertyType property : rootProperty.getValues().get(0).getProperty()) {
                                 if ("Creator".equals(property.getName())) {
-                                    this.applicationName = property.getValues().get(0).getValue().get(0);
+                                    this.applicationName = getPropertyValue(property);
                                 }
                                 if ("Producer".equals(property.getName())) {
-                                    this.applicationVersion = property.getValues().get(0).getValue().get(0);
+                                    this.applicationVersion = getPropertyValue(property);
                                 }
                                 if ("CreationDate".equals(property.getName())) {
-                                    this.applicationCreationDate = property.getValues().get(0).getValue().get(0);
+                                    this.applicationCreationDate = getPropertyValue(property);
+                                }
+                            }
+                        }
+                    }
+                    if ("Objects".equals(rootProperty.getName())) {
+                        this.objectCount = getPropertyValue(rootProperty);
+                    }
+                    if ("Pages".equals(rootProperty.getName())) {
+                        this.pageCount = getPropertyCount(rootProperty);
+                    }
+                    if ("Images".equals(rootProperty.getName())) {
+                        this.imageCoint = getPropertyCount(rootProperty);
+                    }
+                    if ("DocumentCatalog".equals(rootProperty.getName())) {
+                        if (!rootProperty.getValues().isEmpty()) {
+                            for (PropertyType property : rootProperty.getValues().get(0).getProperty()) {
+                                if ("Language".equals(property.getName())) {
+                                    this.language = getPropertyValue(property);
+                                }
+                            }
+                        }
+                    }
+                    if ("Fonts".equals(rootProperty.getName())) {
+                        this.fonts = new HashSet<String>();
+                        for (ValuesType typeValue : rootProperty.getValues()) {
+                            for (PropertyType typeProperty : typeValue.getProperty()) {
+                                for (ValuesType fontValue : typeProperty.getValues()) {
+                                    for (PropertyType fontProporty : fontValue.getProperty()) {
+                                        if ("Font".equals(fontProporty.getName())) {
+                                            for (ValuesType fontsValue : fontProporty.getValues()) {
+                                                for (PropertyType fontsProperty : fontsValue.getProperty()) {
+                                                    if ("BaseFont".equals(fontsProperty.getName())) {
+                                                        String value = getPropertyValue(fontsProperty);
+                                                        if (value != null && !value.isEmpty()) {
+                                                            this.fonts.add(value);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if ("Filters".equals(rootProperty.getName())) {
+                        this.filters = new HashSet<String>();
+                        for (ValuesType typeValue : rootProperty.getValues()) {
+                            for (PropertyType typeProperty : typeValue.getProperty()) {
+                                if ("FilterPipeline".equals(typeProperty.getName())) {
+                                    String value = getPropertyValue(typeProperty);
+                                    if (value != null && !value.isEmpty()) {
+                                        this.filters.add(value);
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+        }
+    }
+
+    private String getPropertyCount(PropertyType property) {
+        try {
+            int size = property.getValues().get(0).getProperty().size();
+            return String.valueOf(size);
+        } catch (Throwable t) {
+            return null;
+        }
+    }
+
+    private String getPropertyValue(PropertyType property) {
+        try {
+            return property.getValues().get(0).getValue().get(0);
+        } catch (Throwable t) {
+            return null;
         }
     }
 
