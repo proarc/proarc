6ccc8c6ee7c91e3cce282fc3a7ce860d3daa1f17
LukasSykora <lukas.sykora@inovatika.cz>
2025-12-31 10:34:39 +0100

    issue #1674: Java 21 - module auth

--- proarc-auth/src/main/java/cz/cas/lib/proarc/authentication/ProarcAuthFilter.java
+++ proarc-auth/src/main/java/cz/cas/lib/proarc/authentication/ProarcAuthFilter.java
@@ -1,42 +1,43 @@
 /*
  * Copyright (C) 2013 Pavel Stastny
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package cz.cas.lib.proarc.authentication;
 
 import cz.cas.lib.proarc.authentication.utils.AuthUtils;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.logging.Logger;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Authentication filter
+ *
  * @author pavels
  */
 public class ProarcAuthFilter implements Filter {
 
     public static final Logger LOGGER = Logger.getLogger(ProarcAuthFilter.class.getName());
-    
+
     public static final String SESSION_KEY = "user";
 
     @Override
@@ -45,21 +46,22 @@ public class ProarcAuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest arg0, ServletResponse arg1,
-            FilterChain chain) throws IOException, ServletException {
+                         FilterChain chain) throws IOException, ServletException {
 
         securedContent(arg0, arg1, chain);
     }
 
     /**
      * Filter secured content
-     * @param arg0 {@link HttpServletRequest}
-     * @param arg1 {@link HttpServletResponse}
+     *
+     * @param arg0  {@link HttpServletRequest}
+     * @param arg1  {@link HttpServletResponse}
      * @param chain {@link FilterChain}
      * @throws IOException
      * @throws ServletException
      */
     public void securedContent(ServletRequest arg0, ServletResponse arg1,
-            FilterChain chain) throws IOException, ServletException {
+                               FilterChain chain) throws IOException, ServletException {
 
         HttpServletRequest httpReq = (HttpServletRequest) arg0;
         HttpServletResponse httpResp = (HttpServletResponse) arg1;
