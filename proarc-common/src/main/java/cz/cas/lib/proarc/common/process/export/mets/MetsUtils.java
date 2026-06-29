ebe58deccae96bf3c5565c3278997716f5e49fcd
LukasSykora <lukas.sykora@inovatika.cz>
2026-06-29 11:45:36 +0200

    issue proarc/proarc#1877: bez prepisu prav pokud slozka uz neexistuje

--- proarc-common/src/main/java/cz/cas/lib/proarc/common/process/export/mets/MetsUtils.java
+++ proarc-common/src/main/java/cz/cas/lib/proarc/common/process/export/mets/MetsUtils.java
@@ -1399,6 +1399,9 @@ public class MetsUtils {
             );
 
     public static void setPermission(File targetFile) throws IOException {
+        if (!targetFile.exists()) {
+            return;
+        }
 
         boolean isWindows = System.getProperty("os.name").toLowerCase().contains("win");
 
